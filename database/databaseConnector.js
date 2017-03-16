812dddcf181654e1a104007b2087d7342bdf2f27
chakr <eljauntywheelie@gmail.com>
2017-03-17 00:26:24 +0530

    dummy mysql connection

--- database/databaseConnector.js
+++ database/databaseConnector.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'chakr',
-	password : '666',
-	database : 'chatapp'
+	host     : '***',
+	user     : '***',
+	password : '***',
+	database : '***'
 });
 
 module.exports = connection;
